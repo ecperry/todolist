058d5c0d9fbbb71b3504550f2cb74b8bbf5af3ad
ecperry <ecperry17@gmail.com>
2017-01-30 09:41:57 -0500

    Fixed functionality on buttons.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ new Vue({
   el: '#app',
 
   methods: {
-//trying to figure out how to push to a new div rather than same card, error on function?
+
+//trying to figure out how to push to a new div rather than same card, error on function? <-- resolved with fixing HTML placement
+// add Card
     addCard: function ()
     {
         this.cards.push({
@@ -18,48 +20,51 @@ new Vue({
           this.titleInputValue = '';
           this.contentInputValue = ''
      },
+
 // trying to make first card pull from array
     sayTitle: function(t)
     {
       return `${t.title}`;
     },
 
-// trying to make first card pull from array, now content
-
+// trying to make first card pull from array, now with content
     sayContent: function(c)
     {
       return `${c.content}`;
     },
-//need a new command other than push, delete isn't working, splice deletes whole list
+
+// delete content
+//need a new command other than push, delete isn't working, splice deletes whole list <--- resolved, no interior function needed
 //trying this but it's not working. from https://forum-archive.vuejs.org/topic/4903/clearing-input-fields
     cancelItem: function ()
     {
-      this.$store.dispatch({
-        clear:''
-      })
-      this.text = ''
+      this.titleInputValue = '';
+      this.contentInputValue = ''
     },
 
-// still having issues here... not sure what's going on. Somehow I'm not matching my object to the function or something?
+// still having issues here... not sure what's going on. Somehow I'm not matching my object to the function or something? <-- resolved with indexOf function to define index
     doneItem: function(card)
     {
-      this.cards.splice(card, 1)
+      var index = this.cards.indexOf(card);
+      this.cards.splice(index, 1)
+    },
+
+//I think that there's something css related that needs to happen here-- dropping this card back to edit format-- maybe div class control? 
+    editItem: function(card)
+    {
     },
 },
 
+//sets input boxes in cards to no text, placeholder text visible (see html), dynamically sets content for first card
   data: {
     titleInputValue: '',
     contentInputValue: '',
     clear: '',
     cards: [
       {
-      title: 'To Do Item',
-      content: 'My first to do item.'
+      title: 'Homework',
+      content: 'Finish exercise 04'
       }
     ]
 }
  });
-
-// add Card
-// delete content
-//
